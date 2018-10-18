41ec6f938226988b17e933265fe41640dde56f65
FediNOP <fedi7003@gmail.com>
2018-10-18 23:04:29 +0500

    Create Phone.java

---
+++ Phone.java
@@ -0,0 +1,25 @@
+class Phone {
+    //2 лаб.
+	
+	class Call{
+		
+	
+		int num;
+		Date date;
+		String state;
+		
+		Call(int num, Date date,String state){
+			
+			this.num = num;
+			this.date = date;
+			this.state = state;
+			
+			
+		}
+		
+		
+		
+	}
+	
+	
+}
